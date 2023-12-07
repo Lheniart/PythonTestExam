82c0b5318c542f64c501bc1f65b9109d75eeea5e
LoanH <lheniart@be-infotech.fr>
2023-12-07 20:56:15 +0100

    feat(pokemon) add pokemon fight

--- app/routers/pokemons.py
+++ app/routers/pokemons.py
@@ -2,6 +2,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter,  Depends
 from app import actions, schemas
+from app.utils.pokeapi import battle_pokemon
 from app.utils.utils import get_db
 
 router = APIRouter()
@@ -15,3 +16,14 @@ def get_pokemons(skip: int = 0, limit: int = 100, database: Session = Depends(ge
     """
     pokemons = actions.get_pokemons(database, skip=skip, limit=limit)
     return pokemons
+@router.get("/battle/{pokemon_api_id_1}/{pokemon_api_id_2}")
+def pokemons_battle(pokemon_api_id_1: int, pokemon_api_id_2: int):
+    """
+        Battle between two pokemons
+        Params:
+            pokemonID (int): id of the first pokemon
+            pokemonID2 (int): id of the second pokemon
+        Return result
+            {"pokemonApiID":pokemonApiID, "result": resultValue:int} (None if draw)
+    """
+    return battle_pokemon(pokemon_api_id_1, pokemon_api_id_2)
--- app/utils/pokeapi.py
+++ app/utils/pokeapi.py
@@ -9,11 +9,13 @@ def get_pokemon_name(api_id):
     """
     return get_pokemon_data(api_id)['name']
 
+
 def get_pokemon_stats(api_id):
     """
         Get pokemon stats from the API pokeapi
     """
-    return False
+    return get_pokemon_data(api_id)['stats']
+
 
 def get_pokemon_data(api_id):
     """
@@ -25,14 +27,26 @@ def get_pokemon_data(api_id):
 def battle_pokemon(first_api_id, second_api_id):
     """
         Do battle between 2 pokemons
+        Return result of battle
+        if there is a winner it return winner id else it return draw
     """
     premierPokemon = get_pokemon_data(first_api_id)
     secondPokemon = get_pokemon_data(second_api_id)
-    battle_result = 0
-    return premierPokemon if battle_result > 0 else secondPokemon if battle_result < 0 else {'winner': 'draw'}
+    if premierPokemon and secondPokemon:
+        battle_result = battle_compare_stats(premierPokemon["stats"], secondPokemon["stats"])
+        if battle_result > 0:
+            return {"Result": first_api_id}
+        if battle_result < 0:
+            return {"Result": second_api_id}
+        return {"Result": "Draw"}
+    return None
 
 
 def battle_compare_stats(first_pokemon_stats, second_pokemon_stats):
     """
         Compare given stat between two pokemons
     """
+    result = 0
+    for i, stats in enumerate(first_pokemon_stats):
+        result += stats["base_stat"] - second_pokemon_stats[i]["base_stat"]
+    return result
