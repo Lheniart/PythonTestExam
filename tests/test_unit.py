534483d0022c3d2b91cf0489e1669a2bb6e50880
Lheniart <loan.heniart@ecoles-epsi.net>
2024-01-11 19:11:10 +0100

    feat(pokemon) add test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -7,20 +7,10 @@ client = TestClient(app)
 
 
 # Test 1: Check the get trainers endpoint
-def test_get_trainers():
-    response = client.get("/trainers")
-    assert response.status_code == 200
-    assert len(response.json()) > 0
+
 
 # 
 # Test 2 : Check the get items endpoint
-def test_get_items():
-    response = client.get("/items")
-    assert response.status_code == 200
-    assert len(response.json()) > 0
+
 
 # Test 3 : Check the get pokemons endpoint
-def test_get_pokemon():
-    response = client.get("/pokemons")
-    assert response.status_code == 200
-    assert len(response.json()) > 0
