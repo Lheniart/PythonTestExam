89482796d73dddcc8226c420bbaa86de135151b0
Tom Saintenoy <tom.saintenoy@ecoles-epsi.net>
2024-01-11 16:39:23 +0100

    Test unit + 1 mock

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,6 +1,26 @@
 from fastapi.testclient import TestClient
 from main import app
 
+
 client = TestClient(app)
+
+# Test 1 : Creation d'un nouveau dresseur + vérification de la création
+def test_create_trainer(mocker):
+    """
+        Creation d'un trainer
+    """
+    mocker.patch(
+        "app.actions.create_trainer",
+        return_value= {
+        "name": "Tom",
+        "birthdate": "1990-11-04",
+        "id": 1,
+        "inventory": [],
+        "pokemons": []
+        }
+    )
+    response = client.post("/trainers/", json={"name": "Tom", "birthdate": "1990-11-04"})
+    assert response.status_code == 200
+    assert response.json() == {"name": "Tom", "birthdate": "1990-11-04", "id": 1, "inventory": [], "pokemons": []}
 
 
