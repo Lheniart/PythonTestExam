3856daf4c9c81093fc9537d3354956cd8cb4da02
Lheniart <loan.heniart@ecoles-epsi.net>
2024-01-11 16:37:45 +0100

    feat(pokemon) add random Pokemon

--- app/routers/pokemons.py
+++ app/routers/pokemons.py
@@ -4,9 +4,9 @@ opérations liées aux pokémons dans une application de formation
 de Pokémon.
 
 Ce routeur expose des endpoints pour récupérer des informations sur les pokémons,
- ainsi qu'une fonction de bataille entre deux pokémons. Les opérations utilisent
-  des fonctions définies dans les modules 'actions', 'schemas', et 'utils',
-   ainsi que des dépendances.
+ainsi qu'une fonction de bataille entre deux pokémons. Les opérations utilisent
+des fonctions définies dans les modules 'actions', 'schemas', et 'utils',
+ainsi que des dépendances.
 
 Classes et objets :
     - router : Instance de APIRouter pour définir les routes de l'API.
@@ -31,14 +31,19 @@ Fonctions :
             - dict : Résultat de la bataille sous forme de dictionnaire.
             {"pokemonApiID": pokemonApiID, "result": resultValue:int}
             (None en cas d'égalité).
-"""
 
+    - pokemons_random() -> List[dict]:
+        Endpoint GET pour obtenir 3 pokémons choisis aléatoirement.
+        Retourne :
+            - List[dict] : Liste de 3 pokémons avec leurs données.
+"""
+from random import randint
 from typing import List
-from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-from app import actions, schemas
-from app.utils.pokeapi import battle_pokemon
+from sqlalchemy.orm import Session
 from app.utils.utils import get_db
+from app import actions, schemas
+from app.utils.pokeapi import battle_pokemon, get_pokemon_data
 
 router = APIRouter()
 
@@ -61,6 +66,21 @@ def pokemons_battle(pokemon_api_id_1: int, pokemon_api_id_2: int):
             pokemonID (int): id of the first pokemon
             pokemonID2 (int): id of the second pokemon
         Return result
-            {"pokemonApiID":pokemonApiID, "result": resultValue:int} (None if draw)
+            {"pokemonApiID": pokemonApiID, "result": resultValue:int} (None if draw)
     """
     return battle_pokemon(pokemon_api_id_1, pokemon_api_id_2)
+
+
+@router.get("/random/")
+def pokemons_random():
+    """
+        Get 3 random pokemons
+        Return:
+            List of 3 pokemons
+    """
+    pokemons: list[dict, dict, dict] = []
+    while len(pokemons) != 3:
+        pokemon = get_pokemon_data(randint(1, 898))
+        if pokemon not in pokemons and pokemon is not None:
+            pokemons.append(pokemon)
+    return pokemons
